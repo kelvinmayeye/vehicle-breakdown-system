2bb005e2a689ac344977ceb00d3353632c4ca53f
Elieth <>
2023-03-25 23:32:01 +0300

    defined relationships on request model

--- app/Models/requests/Request.php
+++ app/Models/requests/Request.php
@@ -2,10 +2,25 @@
 
 namespace App\Models\requests;
 
+
+use App\Models\User;
+use App\Models\services\Service;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Request extends Model
 {
     use HasFactory;
+
+    public function customer(){
+    return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function mechanic(){
+    return $this->belongsTo(User::class, 'mechanic_id');
+    }
+
+    public function service(){
+    return $this->belongsTo(Service::class);
+    }
 }
