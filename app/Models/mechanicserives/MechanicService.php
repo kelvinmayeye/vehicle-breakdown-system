5e7061af1ab4c8a3b9cb251488dff5fea7c46a94
Elieth <>
2023-03-25 23:37:13 +0300

    defined relationship on mechanicserivice

--- app/Models/mechanicserives/MechanicService.php
+++ app/Models/mechanicserives/MechanicService.php
@@ -2,10 +2,23 @@
 
 namespace App\Models\mechanicserives;
 
+
+use App\Models\User;
+use App\Models\services\Service;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MechanicService extends Model
 {
     use HasFactory;
+
+
+    public function mechanic(){
+    return $this->belongsTo(User::class, 'mechanic_id');
+    }
+
+    public function service(){
+    return $this->belongsTo(Service::class);
+    }
+
 }
--- app/Models/payments/Payment.php
+++ app/Models/payments/Payment.php
@@ -2,10 +2,22 @@
 
 namespace App\Models\payments;
 
+
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
     use HasFactory;
+
+
+    public function customer(){
+    return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    public function mechanic(){
+    return $this->belongsTo(User::class, 'mechanic_id');
+    }
+
 }
